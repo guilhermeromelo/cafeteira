fad558ebd40e1c23facc6054d19b7256682fca01
Guilherme Rodrigues de Melo <guilhermeromelo@gmail.com>
2023-06-18 21:22:57 -0300

    feat: Adicionando telas 3, 4 e 5, controle do servo motor e reles, melhorando print temperatura e peso

--- cafeteira/cafeteira.ino
+++ cafeteira/cafeteira.ino
@@ -2,7 +2,9 @@
 const int tamanhoPadrao=1;
 const int intensidadePadrao=0;
 const int debounceDelay = 1500;
-const int tempoAtualizacaoPeso = 2000;
+const int tempoAtualizacaoInfoTela1 = 2000;
+const int tempoAtualizacaoInfoTela4 = 1000;
+const int tempoPularProximaTela = 5000;
 //FIM CONFIG --------------------------------------------------------
 
 
@@ -16,16 +18,23 @@ const int tempoAtualizacaoPeso = 2000;
 #include <DallasTemperature.h>
 //CELULA DE CARGA
 #include "HX711.h"
+//Carrega a biblioteca Servo
+#include "Servo.h"
 //FIM BIBLIOTECAS ---------------------------------------------------
 
 
 
 //PINAGEM -----------------------------------------------------------
 //CELULA DE CARGA
-#define DT A0
-#define SCK A1
+#define DT A3
+#define SCK A2
 //SENSOR DE TEMPERATURA
-#define pinSensorTemp 7
+#define pinSensorTemp A0
+//RELES
+#define releResistencia 8
+#define releBombaAgua 9
+//SERVO MOTOR DISPENSER CAFE
+#define pinServoCafe 10
 //FIM PINAGEM -------------------------------------------------------
 
 
@@ -35,7 +44,9 @@ LiquidCrystal_I2C lcd(0x27,20,4); // DEFINE O ENDEREÇO UTILIZADO PELO ADAPTADOR
 OneWire ourWire(pinSensorTemp); //CONFIGURA UMA INSTÂNCIA ONEWIRE PARA SE COMUNICAR COM O SENSOR DE TEMPERATURA
 DallasTemperature sensorTemperatura(&ourWire); //BIBLIOTECA DallasTemperature UTILIZA A OneWire
 HX711 escala; //CRIA A VARIÁVEL DE ESCALA DE MEDIÇÃO DA CELULA DE CARGA
+Servo servo; // Criar um Objeto Servo
 float peso = 0.0;
+float temperatura = 0.0;
 //BOTOES-------------------------------
 // 0 - Botao Voltar
 // 1 - Botao Seta Esquerda (<---)
@@ -68,12 +79,14 @@ void printHomePage(){
   lcd.setCursor(1, 2);
   lcd.print("FAZER SEU CAFE...");
   lcd.setCursor(0, 3);
-  lcd.print("TRES=   C PESO=    g");
+  lcd.print("T   C A   % P      g");
 }
 
 void atualizarInfoTela1(){
-  lcd.setCursor(15, 3);
-  lcd.print(peso*1000, 0);
+  lcd.setCursor(13, 3);
+  deslocarDireitaPeso(peso*1000);
+  lcd.setCursor(1,3);
+  deslocarDireitaNumeroInteiro3Caracteres(temperatura);
 }
 //FIM TELA 1 --------------------------
 //TELA 2 ------------------------------
@@ -116,10 +129,80 @@ void atualizar_cursor_tela2(){
 //TELA 3 ------------------------------
 void printTela3(){
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Tela 3");
+  lcd.setCursor(2, 0);
+  lcd.print("OTIMA ESCOLHA!!");
+  lcd.setCursor(0, 1);
+  lcd.print("INICIANDO PREPARO...");
+  lcd.setCursor(3, 2);
+  lcd.print("MUITO OBRIGADO");
+  lcd.setCursor(0, 3);
+  lcd.print("PELA SUA PREFERENCIA");
 }
 //FIM TELA 3 --------------------------
+//TELA 4 ------------------------------
+void printTela4(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("PREPARANDO SEU CAFE!");
+  lcd.setCursor(0, 1);
+  lcd.print("STATUS: ADICIO. CAFE");
+  lcd.setCursor(3, 2);
+  lcd.print("PESO=       g");
+  lcd.setCursor(0, 3);
+  lcd.print("T. RESISTENCIA=    C");
+}
+void atualizarInfoTela4(){
+  lcd.setCursor(9, 2);
+  deslocarDireitaPeso(peso*1000);
+  lcd.setCursor(16,3);
+  deslocarDireitaNumeroInteiro3Caracteres(temperatura);
+}
+//FIM TELA 4 --------------------------
+//TELA 5 ------------------------------
+void printTela5(){
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("SEU CAFE ESTA PRONTO");
+  lcd.setCursor(3, 2);
+  lcd.print("VOLTE SEMPRE!!");
+}
+//FIM TELA 5 --------------------------
+
+void deslocarDireitaNumeroInteiro3Caracteres(float num){
+  if(num < 10) lcd.print("  ");
+  else if(num < 100) lcd.print(" ");
+  
+  lcd.print(num,0);
+}
+
+void deslocarDireitaPeso(float peso){
+  if(peso < -999){
+    lcd.print("NEGAT.");
+  } else {
+    if(peso < -99){
+      //nada
+    } else if(peso < -9 && peso > -100){
+      lcd.print(" ");
+    } else if(peso < -0){
+      lcd.print("  ");
+    } else if(peso < 10){
+      lcd.print("   ");
+    } else if(peso < 100){
+      lcd.print("  ");
+    } else if(peso < 1000){
+      lcd.print(" ");
+    }
+    lcd.print(peso,1);
+  }
+}
+
+void esperarTrocaTela(int proximaTela){
+  if((loopMillis == 0 || (millis() - loopMillis) > tempoPularProximaTela) || isButtonPressed(3)){
+    tela = proximaTela;
+    mudarTela();
+  }
+}
+
 void mudarTela(){
   if(tela == 1) printHomePage();
   else if (tela == 2) {
@@ -127,7 +210,18 @@ void mudarTela(){
     tamanho = tamanhoPadrao;
     intensidade = intensidadePadrao;
     printTela2();
-  } else if (tela == 3) printTela3();
+  } else if (tela == 3) {
+    atualizarValorLoopMillis();
+    printTela3();
+  } 
+  else if (tela == 4) {
+    atualizarValorLoopMillis();
+    printTela4();
+  }
+  else if (tela == 5) {
+    atualizarValorLoopMillis();
+    printTela5();
+  }
 }
 //FIM FUNCOES TELA --------------------------------------------------
 
@@ -198,15 +292,43 @@ void monitorarBotoesTela2(){
 
 
 
-//FUNCOES SENSORES --------------------------------------------------
+//FUNCOES SENSORES E ATUADORES --------------------------------------
 void atualizarPeso(){
-  escala.power_up();  
+  escala.power_up();
   peso = escala.get_units();
   escala.power_down();
 }
-//FIM FUNCOES SENSORES --------------------------------------------------
 
+void atualizarTemperatura(){
+  sensorTemperatura.requestTemperatures();//SOLICITA QUE A FUNÇÃO INFORME A TEMPERATURA DO SENSOR
+  temperatura = sensorTemperatura.getTempCByIndex(0);
+}
 
+void switchServoCafe(bool ligar){
+  if(ligar == true)
+    servo.write(130);
+  else
+    servo.write(90);
+}
+
+void switchResistencia(bool ligar){
+  if(ligar == true)
+    digitalWrite(releResistencia, HIGH);
+  else
+    digitalWrite(releResistencia, LOW);
+}
+
+void switchBombaAgua(bool ligar){
+  if(ligar == true)
+    digitalWrite(releBombaAgua, HIGH);
+  else
+    digitalWrite(releBombaAgua, LOW);
+}
+//FIM FUNCOES SENSORES E ATUADORES --------------------------------------
+
+void atualizarValorLoopMillis(){
+  loopMillis = millis();
+}
 
 void setup() {
   Serial.begin(9600); //INICIALIZA A SERIAL
@@ -214,7 +336,13 @@ void setup() {
   //PINAGEM
   for(int i=0;i<5;i++){
     pinMode(buttons[i],INPUT_PULLUP);
-  }  
+  }
+
+  pinMode(releBombaAgua, OUTPUT);
+  pinMode(releResistencia, OUTPUT);
+
+  //ASSOCIAR PORTA DO SERVO A VARIAVEL SERVO
+  servo.attach(pinServoCafe);
 
   //PREPARACAO CELULA DE CARGA
   escala.begin (DT, SCK); //INICIA CONTROLE CELULA DE CARGA
@@ -232,25 +360,43 @@ void setup() {
 }
  
 void loop() {
-  //MONITORAR SENSORES E MOSTRAR NO DISPLAY - DEPENDE DO TEMPO DE ATUALIZAÇÃO
-  if((tela == 1) //|| tela fabricando o café .....
-    && (loopMillis == 0 || (millis() - loopMillis) > tempoAtualizacaoPeso)){
-    loopMillis = millis();
-    atualizarPeso();
-    atualizarInfoTela1();
-  }
-
-  //MONITORAR BOTOES E REAGIR
-  if(tela == 1){
-    //HOME PAGE
-    monitorarBotoesTela1();
-  } else if(tela == 2){
-    //SELECIONAR CAFE
-    monitorarBotoesTela2();
-  } else {
-    if(isButtonPressed(0)){//SOMENTE PARA TESTE
-      tela --;
-      mudarTela();
+  if(tela == 4){
+    if((loopMillis == 0 || (millis() - loopMillis) > tempoAtualizacaoInfoTela4)){
+      atualizarValorLoopMillis();
+      atualizarPeso();
+      atualizarTemperatura();
+      atualizarInfoTela4();
     }
+
+
+    switchResistencia(true);
+    switchServoCafe(true);
+    delay(6000);
+
+    switchServoCafe(false);
+    switchBombaAgua(true);
+    delay(6000);
+
+    switchResistencia(false);    
+    switchBombaAgua(false);
+
+    delay(2000);
+    tela++;
+    mudarTela();
+
+  } else {
+    //MONITORAR SENSORES E MOSTRAR NO DISPLAY - DEPENDE DO TEMPO DE ATUALIZAÇÃO
+    if(tela == 1 && (loopMillis == 0 || (millis() - loopMillis) > tempoAtualizacaoInfoTela1)){
+      atualizarValorLoopMillis();
+      atualizarPeso();
+      atualizarTemperatura();
+      atualizarInfoTela1();
+    }
+
+    //MONITORAR BOTOES E REAGIR
+    if(tela == 1) monitorarBotoesTela1(); //HOME PAGE
+    else if(tela == 2) monitorarBotoesTela2(); //SELECIONAR CAFE
+    else if(tela == 3) esperarTrocaTela(4); //INICIANDO PREPARO CAFE
+    else if(tela == 5) esperarTrocaTela(1); //CAFE PRONTO
   }
 }
